73c81bc9de03a16b7d8c038f0bf51c9e1ac4e0b9
thelazarbeam <eddie.lazar@yahoo.com>
2019-01-18 15:06:18 -0500

    plan to show commute time. hard WIP

---
+++ location.js
@@ -0,0 +1,9 @@
+// function showPosition(position) {
+//   return [position.coords.latitude, position.coords.longitude]; 
+// }
+
+// var homeLocation = navigator.geolocation.getCurrentPosition(showPosition);
+
+var homeLocation = [40.286017, -74.311372];
+
+var workLocation = [40.645277, -74.574852];
